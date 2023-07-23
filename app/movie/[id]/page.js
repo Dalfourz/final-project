eafa86bbbf214d2587db6f9a0ec7b5503edf006f
Dalfour <yjulius.mail@gmail.com>
2023-07-24 09:58:45 +1000

    working on actors scetion

--- app/movie/[id]/page.js
+++ app/movie/[id]/page.js
@@ -4,11 +4,12 @@ import MovieImage from "@/components/MovieImage";
 import axios from "axios";
 import VideoPlayer from "@/components/VideoPlayer";
 import CurrencyFormatter from "@/components/CurrencyFormatter";
+import { People } from "@mui/icons-material";
 
 async function MoviePage({ params: { id } }) {
   const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
   const OAUTH = process.env.NEXT_PUBLIC_OAUTH;
-  const axios = require("axios");
+  console.log(id)
 
   const options = {
     method: "GET",
@@ -28,42 +29,81 @@ async function MoviePage({ params: { id } }) {
     },
   };
 
-  axios
-    .request(options)
-    .then(function (response) {
-      // console.log(response.data);
-    })
-    .catch(function (error) {
-      console.error(error);
-    });
+  try {
+    // First API call
+    const response = await axios.request(options);
+    const people = response.data;
+    console.log(people.cast.slice(0,4))
 
-  axios
-    .request(options2)
-    .then(function (response) {
-      // console.log(response.data);
-    })
-    .catch(function (error) {
-      console.error(error);
-    });
-    
-  // const movie = options2;
+    // Second API call
+    const response2 = await axios.request(options2);
+    const movie = response2.data;
 
-  // console.log(response)
+    const officalVideos = movie.videos;
+    const video = officalVideos.results;
+    const trailers = video.filter((x) => x.type === "Trailer");
 
-  // const officalVideos = movie.videos;
-  // const video = officalVideos.results;
-  // const trailers = video.filter((x) => x.type === "Trailer");
+    return (
+      <div className="my-6 mx-8">
+        <div className="md:flex mb-4">
+          <div className="relative md:w-[calc(100%/2)] h-screen md:mr-8">
+            <MovieImage movie={movie} fill className="" />
+          </div>
+          <div className="md:w-[calc(100%/2)] text-center">
+            <h1 className="font-bold text-xl">{movie.title}</h1>
+            <h2 className="">{movie.tagline}</h2>
+            <p className="mb-2">{movie.overview}</p>
+            <p className="mb-2">Release Date: {movie.release_date}</p>
+            <div>
+              <p className="font-semibold">Budget:</p>
+              <CurrencyFormatter movie={movie.budget} />
+            </div>
+            <div>
+              <p className="font-semibold">Revenue:</p>
+              <CurrencyFormatter movie={movie.revenue} />
+            </div>
+            <div>
+              <p>Director: </p>
+            </div>
+            <div>Actors:</div>
+            <ul>
+              <li>
 
-  // const people = data2
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="flex-col">
+          <h2 className="text-lg font-semibold mb-2 text-center">
+            Video Clips & Trailers
+          </h2>
+          <div
+            className={
+              trailers.length === 1
+                ? "m-auto w-full max-w-2xl "
+                : "md:grid grid-flow-col md:grid-cols-3  "
+            }
+          >
+            {trailers.slice(0, 4).map((video) => (
+              <div className="mb-4 m-auto">
+                <VideoPlayer video={video} className="" />
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    );
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return <p>"Error fetching data"</p>;
+  }
+}
 
-  // const directors = people.credits
+export default MoviePage;
 
-  // console.log(people)
 
-  // const officialTrailer = filterByValue(trailers, "official" || "Trailer")
-  // console.log(trailers)
-  // console.log(officialTrailer);
 
+  
   // function filterByValue(array, string) {
   //   return array.filter((o) =>
   //     Object.keys(o).some((k) => {
@@ -73,46 +113,6 @@ async function MoviePage({ params: { id } }) {
   //   );
   // }
 
-  // console.log(officialTrailer);
+  // return (
 
-    // return (
-    //   <div className="my-6 mx-8">
-    //     <div className="md:flex mb-4">
-    //       <div className="relative md:w-[calc(100%/2)] h-screen md:mr-8">
-    //         <MovieImage movie={movie} fill className="" />
-    //       </div>
-    //       <div className="md:w-[calc(100%/2)] text-center">
-    //         <h1 className="font-bold text-xl">{movie.title}</h1>
-    //         <h2 className="">{movie.tagline}</h2>
-    //         <p className="mb-2">{movie.overview}</p>
-    //         <p className="mb-2">Release Date: {movie.release_date}</p>
-    //         <div>
-    //           <p className="font-semibold">Budget:</p>
-    //           <CurrencyFormatter movie={movie.budget} />
-    //         </div>
-    //         <div>
-    //           <p className="font-semibold">Revenue:</p>
-    //           <CurrencyFormatter movie={movie.revenue} />
-    //         </div>
-    //         <div>
-    //           <p>Director: </p>
-    //         </div>
-    //       </div>
-    //     </div>
-    //     <div className="flex-col">
-    //       <h2 className="text-lg font-semibold mb-2 text-center">
-    //         Video Clips & Trailers
-    //       </h2>
-    //       <div className={trailers.length === 1 ? "m-auto w-full max-w-2xl " : "md:grid grid-flow-col md:grid-cols-3  "}>
-    //         {trailers.slice(0, 4).map((video) => (
-    //           <div className="mb-4 m-auto">
-    //             <VideoPlayer video={video} className="" />
-    //           </div>
-    //         ))}
-    //       </div>
-    //     </div>
-    //   </div>
-    // );
-}
-
-export default MoviePage;
+  // );
