bb721be69355a202f76a8d87ef61bc8070af8461
Dalfour <yjulius.mail@gmail.com>
2023-08-08 10:19:16 +1000

    fixed sticky footer back to the bottom of the page

--- components/Footer.js
+++ components/Footer.js
@@ -5,7 +5,7 @@ import { faFilm } from "@fortawesome/free-solid-svg-icons";
 
 function Footer() {
   return (
-    <section className="bg-black text-white text-center py-4 w-screen lg:flex lg:justify-around lg:items-center fixed bottom-0 mt-auto overflow-scroll">
+    <section className="bg-black text-white text-center py-4 w-screen lg:flex lg:justify-around lg:items-center bottom-0 mt-auto overflow-scroll">
       <div className="lg:flex">
         <div className="flex hover:cursor-pointer justify-center md:m-auto mb-2">
           <FontAwesomeIcon icon={faFilm} className="w-10 h-10 mr-2" />
